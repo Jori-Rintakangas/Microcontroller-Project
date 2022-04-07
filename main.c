76e28f7d0308c7a5e2fd508a1628c8b7400b5818
Jori Rintakangas <jori.rintakangas@tuni.fi>
2022-04-07 16:09:17 +0300

    Temperature setting by USART

--- main.c
+++ main.c
@@ -26,8 +26,11 @@
 #define TEMP_MAX 255.0
 #define VOLTAGE_DIV 5000.0/105000
 
+#define ENTER_KEY 13
+#define BUFFER_SIZE 4
 
 #define SYSTEM_SLEEP (PIND & (1 << PIND2)) == 0
+#define NEW_SETTING_RECEIVED (UCSR0A & (1 << RXC0)) != 0
 
 uint8_t setting;
 uint8_t reading;
@@ -40,6 +43,26 @@ ISR(PCINT2_vect) { }
 ISR(INT1_vect)
 {
 	temp_source = PIND & (1 << PIND3);
+}
+
+/* Reading temperature setting send by virtual terminal */
+void receive_USART()
+{
+	if (NEW_SETTING_RECEIVED)
+	{
+		int i = 0;
+		char buffer[BUFFER_SIZE];
+		char c = '\0';
+		while (c != ENTER_KEY) // Receive characters until enter pressed
+		{
+			// Wait until data receiving is ready
+			while ( !(UCSR0A & (1 << RXC0)));
+			c = UDR0;
+			buffer[i] = c;
+			i++;
+		}
+		setting = atoi(buffer);
+	}
 }
 
 /* Reading an analog-to-digital converter result from given channel */
@@ -58,13 +81,7 @@ uint8_t read_ADC(uint8_t channel)
 }
 
 int main()
-{
-	DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2);
-	// Turning on all LEDs for 2 seconds in the beginning of program
-	PORTB |= (1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2);
-	_delay_ms(2000);
-	PORTB &= ~(1 << PORTB0) & ~(1 << PORTB1) & ~(1 << PORTB2);
-	
+{	
 	temp_source = PIND & (1 << PIND3);
 	
 	init_switches();
@@ -87,30 +104,42 @@ int main()
 		sei(); // enable interrupts	
 		
 		
+		
 		// Reading selected temperature and true temperature
-		setting = read_ADC(CONTROL);
+		if (temp_source == TRIMMER)
+		{
+			setting = read_ADC(CONTROL);
+		}
+		else
+		{	
+			receive_USART();
+		}
+		
+		
 		reading = read_ADC(HEATER);
 				
-		// Setting value for PWM to control	heating element
+		// Setting value for PWM to control heating element
 		OCR0A = setting;
 				
 		int setting_pct = round((setting / TEMP_MAX) * 100.0);
 		int reading_pct = round((reading / TEMP_MAX) * 100.0);
 		int diff_pct = round(VOLTAGE_DIV * (setting / TEMP_MAX) * 100.0);
 						
-		PORTB &= ~(1 << PORTB0) & ~(1 << PORTB1) & ~(1 << PORTB2);
 		if (setting_pct <= reading_pct + diff_pct + 1
 			&& setting_pct >= reading_pct + diff_pct - 1)
 		{
 			PORTB |= (1 << PORTB1);
+			PORTB &= ~(1 << PORTB0) & ~(1 << PORTB2);
 		}
 		else if (setting_pct > reading_pct + diff_pct)
 		{
 			PORTB |= (1 << PORTB2);
+			PORTB &= ~(1 << PORTB0) & ~(1 << PORTB1);
 		}
 		else
 		{
 			PORTB |= (1 << PORTB0);
+			PORTB &= ~(1 << PORTB1) & ~(1 << PORTB2);
 		}		
     }
 }
